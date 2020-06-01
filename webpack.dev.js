fdae3ebf6f0a4a6e92fe17f98f65fdab73c44f7c
Christopher Adams <christopher@cxadams.com>
2020-06-01 12:11:44 +0800

    Split out common webpack config

---
+++ webpack.dev.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, '_site')
+  },
+  devtool: 'inline-source-map',
+});
